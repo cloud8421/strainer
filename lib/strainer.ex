040e9209f588cb637989c1dc4768581fe9550651
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-05-11 18:56:00 +0100

    Comments cleanup.

--- lib/strainer.ex
+++ lib/strainer.ex
@@ -1,8 +1,6 @@
 defmodule Strainer do
   use Application.Behaviour
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
   def start(_type, config) do
     Strainer.Supervisor.start_link config
   end
--- lib/strainer/static_handler.ex
+++ lib/strainer/static_handler.ex
@@ -1,9 +1,5 @@
 defmodule Strainer.StaticHandler do
   @behaviour :cowboy_http_handler
-  @moduledoc """
-  This is an HTTP handler that will serve a single file to the
-  client.
-  """
 
   def init({_any, :http}, req, []) do
     { :ok, req, :undefined}
